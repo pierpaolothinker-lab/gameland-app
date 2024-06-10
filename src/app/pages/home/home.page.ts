bf190f4106d4174e4eb4f1f6e4ee88e9f57a00aa
Pierpaolo Monaco <pierpaolo.monaco@gmail.com>
2024-06-10 16:53:28 -0400

    add home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCardContent, IonIcon, IonCardHeader, IonCardSubtitle, IonCard, IonList, IonItem, IonAvatar, IonLabel, IonButtons, IonMenuButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import {addIcons} from "ionicons";
+import { chevronForwardOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardContent,IonIcon, IonCardHeader, IonCardSubtitle, IonCard, IonList, IonItem, IonAvatar, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton]
 })
 export class HomePage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    addIcons({ chevronForwardOutline });
   }
 
 }
